792e7a98e20fb2976b601569e93c7b119a693dd4
EmmaRaijmakers <71829294+EmmaRaijmakers@users.noreply.github.com>
2023-02-07 15:22:23 +0100

    Added color range to keep_red function

--- Opdracht_1/Opdrachten Kleuren, Histogrammen en Features.py
+++ Opdracht_1/Opdrachten Kleuren, Histogrammen en Features.py
@@ -10,11 +10,11 @@ img = io.imread("Opdracht_1/flower.jpg")
 #dan pixels aangegeven met 1 omzetten en dan samenvoegen (matrix vermenigvuldiging)
 
 #function that converts image img to grayscale but keeps red pixels with a R-value
-#greater than the threshold thres
-def keep_red(img, thres):
+#within the threshold values min and max
+def keep_red(img, thres_min, thres_max):
     for row in img:
         for pixel in row:
-            if(not(pixel[0] > thres and pixel[0] > pixel[1] and pixel[0] > pixel[2])):
+            if(not(pixel[0] >= thres_min and pixel[0] <= thres_max and pixel[0] > pixel[1] and pixel[0] > pixel[2])):
                 grayscale_red = pixel[0] * 0.299
                 grayscale_green = pixel[1] * 0.587
                 grayscale_blue = pixel[2] * 0.114
@@ -40,7 +40,7 @@ def img_to_hue_histogram(img):
 img_to_hue_histogram(img)
 
 #show grayscale image
-keep_red(img, 75)
+keep_red(img, 75, 255)
 viewer = ImageViewer(img)
 viewer.show()
 
