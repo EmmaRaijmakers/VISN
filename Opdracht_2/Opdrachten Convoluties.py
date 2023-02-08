12a62b6628b67eec13192d3472f70bfbd81a4ea5
EmmaRaijmakers <71829294+EmmaRaijmakers@users.noreply.github.com>
2023-02-08 14:33:14 +0100

    Added 3 skimage filters

--- Opdracht_2/Opdrachten Convoluties.py
+++ Opdracht_2/Opdrachten Convoluties.py
@@ -9,6 +9,7 @@ viewer.show()
 
 mask1=[[1/9,1/9,1/9],[1/9,1/9,1/9],[1/9,1/9,1/9]]
 
+#Opdracht 1
 mask_edgedetection1 = [[-1,0,1],[-1,0,1],[-1,0,1]]
 mask_edgedetection2 = [[-1,-1,-1],[0,0,0],[1,1,1]]
 mask_edgedetection3 = [[-1,-1,-1],[-1,8,-1],[-1,-1,-1]]
@@ -27,3 +28,25 @@ viewer.show()
 newimage3 = scipy.ndimage.convolve(image, mask_edgedetection3)
 viewer = ImageViewer(newimage3)
 viewer.show()
+
+#Opdracht 2
+
+#Wat opvalt aan onderstaande filters in vergelijking met bovenstaande,
+#is dat onderstaande filters veel beter werken. 
+
+#Prewitt
+prewitt_filter = filters.prewitt(image)
+viewer = ImageViewer(prewitt_filter)
+viewer.show()
+
+#Scharr
+scharr_filter = filters.scharr(image)
+viewer = ImageViewer(scharr_filter)
+viewer.show()
+
+#Sobel
+sobel_filter = filters.sobel(image)
+viewer = ImageViewer(sobel_filter)
+viewer.show()
+
+#Opdracht 3
