0424512936b75e0220702068be299ae644066c08
EmmaRaijmakers <71829294+EmmaRaijmakers@users.noreply.github.com>
2023-03-13 17:19:33 +0100

    added gaussian filter and viewer

--- Eindopdracht/src/main.py
+++ Eindopdracht/src/main.py
@@ -1,6 +1,8 @@
+from skimage.viewer import ImageViewer
 from skimage.io import imread
 from skimage.color import rgb2gray
 from skimage import feature
+from skimage.filters import gaussian
 
 image = imread("hier_image_path")
 
@@ -9,3 +11,9 @@ image_gray = rgb2gray(image)
 
 #edge detection
 canny_filter = feature.canny(image, sigma=2.1) #TODO change sigma
+
+#gaussian filter                    #for rgb image
+gaussian_filter = gaussian(image, multichannel=True, sigma=2) #TODO change sigma
+
+viewer = ImageViewer(image)
+viewer.show()
