67f3faf77eec830c38c09417796a758d0cab6a9e
EmmaRaijmakers <71829294+EmmaRaijmakers@users.noreply.github.com>
2023-03-13 16:34:03 +0100

    added edge detection

--- Eindopdracht/src/main.py
+++ Eindopdracht/src/main.py
@@ -1,7 +1,11 @@
 from skimage.io import imread
 from skimage.color import rgb2gray
+from skimage import feature
 
 image = imread("hier_image_path")
 
 #grayscale
 image_gray = rgb2gray(image)
+
+#edge detection
+canny_filter = feature.canny(image, sigma=2.1) #TODO change sigma
