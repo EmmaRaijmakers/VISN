5991774b00637c64f9f8825a8788581f53c8a4d5
EmmaRaijmakers <71829294+EmmaRaijmakers@users.noreply.github.com>
2023-03-15 17:30:04 +0100

    added code to load ASL images to training data list

--- Eindopdracht/src/main.py
+++ Eindopdracht/src/main.py
@@ -1,8 +1,19 @@
+#TODO remove libraries that are not used
+
 from skimage.viewer import ImageViewer
 from skimage.io import imread
 from skimage.color import rgb2gray
 from skimage import feature
 from skimage.filters import gaussian
+
+import numpy as np
+import matplotlib.pyplot as plt
+import os
+import cv2
+
+#TODO write read me and give credit in read me to \/
+
+#Dataset used for this project from: https://www.kaggle.com/datasets/grassknoted/asl-alphabet?resource=download
 
 image = imread("Opdracht_1/flower.jpg")
 
@@ -15,8 +26,8 @@ canny_filter = feature.canny(image_gray, sigma=2.1) #TODO change sigma
 #gaussian filter                    #for rgb image
 gaussian_filter = gaussian(image, multichannel=True, sigma=2) #TODO change sigma
 
-viewer = ImageViewer(image)
-viewer.show()
+# viewer = ImageViewer(image)
+# viewer.show()
 
 # viewer = ImageViewer(image_gray)
 # viewer.show()
@@ -27,3 +38,33 @@ viewer.show()
 # viewer = ImageViewer(gaussian_filter)
 # viewer.show()
 
+dataset_dir = "Eindopdracht/dataset/asl_alphabet_train/asl_alphabet_train"
+letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'del', 'nothing', 'space']
+
+#tutorial: https://www.youtube.com/watch?v=j-3vuBynnOE
+
+#TODO split training set in training and test dataset
+training_data = []
+
+for letter in letters:
+    #get directory of a certain letter
+    path = os.path.join(dataset_dir, letter)
+
+    #create number for each letter
+    letter_num = letters.index(letter)
+    print(letter)
+    
+    for image in os.listdir(path):
+        #get one image
+        image_array = cv2.imread(os.path.join(path, image), cv2.IMREAD_GRAYSCALE) #TODO gray scale can be added here, do it??
+
+        #compress the image to a smaller resolution
+        #TODO is this needed?? > makes faster
+        image_size = 50 #TODO this bigger/smaller?
+        compressed_image_array = cv2.resize(image_array, (image_size, image_size))
+
+        #add new image to the training set
+        training_data.append([compressed_image_array, letter_num])
+
+
+print(len(training_data))
