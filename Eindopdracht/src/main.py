9b750c4c1e3a31ff48acb4949235cc06c4e4f8bb
EmmaRaijmakers <71829294+EmmaRaijmakers@users.noreply.github.com>
2023-03-15 15:31:23 +0100

    Tested preprocessing on test image
    Tested the preprocessing on the test image 'flower.jpg' and viewed the resulting images with the image viewer

--- Eindopdracht/src/main.py
+++ Eindopdracht/src/main.py
@@ -4,16 +4,26 @@ from skimage.color import rgb2gray
 from skimage import feature
 from skimage.filters import gaussian
 
-image = imread("hier_image_path")
+image = imread("Opdracht_1/flower.jpg")
 
 #grayscale
 image_gray = rgb2gray(image)
 
 #edge detection
-canny_filter = feature.canny(image, sigma=2.1) #TODO change sigma
+canny_filter = feature.canny(image_gray, sigma=2.1) #TODO change sigma
 
 #gaussian filter                    #for rgb image
 gaussian_filter = gaussian(image, multichannel=True, sigma=2) #TODO change sigma
 
 viewer = ImageViewer(image)
 viewer.show()
+
+# viewer = ImageViewer(image_gray)
+# viewer.show()
+
+# viewer = ImageViewer(canny_filter)
+# viewer.show()
+
+# viewer = ImageViewer(gaussian_filter)
+# viewer.show()
+
