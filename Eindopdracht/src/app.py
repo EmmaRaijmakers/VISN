8dd6f147ebbfd32117d8d374095281b4062b7de0
EmmaRaijmakers <71829294+EmmaRaijmakers@users.noreply.github.com>
2023-04-03 18:14:11 +0200

    Added preprocessing of the captured image

--- Eindopdracht/src/app.py
+++ Eindopdracht/src/app.py
@@ -1,11 +1,14 @@
 import cv2
 from keras.models import load_model
+from skimage import color
 
 #load model made in main file
 #to not have to train the model everytime the application is run
 
 #TODO change to relative paths (why not working?)
 model = load_model("C:/Users/emmar/Documents/GitHub/VISN/Eindopdracht/src/ASL_model.keras")
+
+image_size = 50
 
 webcam_port = 0
 webcam = cv2.VideoCapture(webcam_port)
@@ -22,7 +25,11 @@ if result:
     cv2.waitKey(0)
     cv2.destroyWindow("ASL-detection")
 
-
+    #preprocess the image the same as the images used for training
+    gray_image = color.rgb2gray(image)
+    compressed_image = cv2.resize(image, (image_size, image_size))
+    reshaped_image = compressed_image.reshape(-1, image_size, image_size, 1)
+    normalized_image = (reshaped_image / 255) - 0.5
   
 #if capture went wrong
 else:
