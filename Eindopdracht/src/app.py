9656fa85d8cfc8b973fd760234765028ea0668cc
EmmaRaijmakers <71829294+EmmaRaijmakers@users.noreply.github.com>
2023-04-03 16:51:10 +0200

    Added code to save model and load in application

--- Eindopdracht/src/app.py
+++ Eindopdracht/src/app.py
@@ -1,4 +1,11 @@
 import cv2 as cv
+from keras.models import load_model
+
+#load model made in main file
+#to not have to train the model everytime the application is run
+
+#TODO change to relative paths (why not working?)
+model = load_model("C:/Users/emmar/Documents/GitHub/VISN/Eindopdracht/src/ASL_model.keras")
 
 webcam_port = 0
 webcam = cv.VideoCapture(webcam_port)
@@ -18,3 +25,5 @@ if result:
 #if capture went wrong
 else:
     print("No image was captured")
+
+#load_model van checkpoint om model op te slaan en weer te loaden, zodat niet bij opstarten model te trainen
