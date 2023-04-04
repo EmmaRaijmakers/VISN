4cb6d05fcc93e92d03cb3b43d35076fbae71702f
EmmaRaijmakers <71829294+EmmaRaijmakers@users.noreply.github.com>
2023-04-04 17:25:17 +0200

    Added code to predict captured ASL-images dataset

--- Eindopdracht/src/app.py
+++ Eindopdracht/src/app.py
@@ -13,37 +13,60 @@ letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
 
 image_size = 50
 
-webcam_port = 0
-webcam = cv2.VideoCapture(webcam_port)
-  
-#reading the input using the webcam
-result, image = webcam.read()
-  
-#if image is captured  
-if result: 
-    #show the capture
-    cv2.imshow("ASL-detection", image)
-  
-    #destroy window after keyboard interrupt
-    cv2.waitKey(0)
-    cv2.destroyWindow("ASL-detection")
+live_capture_mode = False
 
-    #preprocess the image the same as the images used for training
-    gray_image = color.rgb2gray(image)
-    compressed_image = cv2.resize(gray_image, (image_size, image_size))
-    reshaped_image = compressed_image.reshape(-1, image_size, image_size, 1)
-    normalized_image = (reshaped_image / 255) - 0.5
+if live_capture_mode:
+    webcam_port = 0
+    webcam = cv2.VideoCapture(webcam_port)
+    
+    #reading the input using the webcam
+    result, image = webcam.read()
+    
+    #if image is captured  
+    if result: 
+        #show the capture
+        cv2.imshow("ASL-detection", image)
+    
+        #destroy window after keyboard interrupt
+        cv2.waitKey(0)
+        cv2.destroyWindow("ASL-detection")
 
-    #predict the letter
-    prediction = model.predict(normalized_image)
+        #preprocess the image the same as the images used for training
+        gray_image = color.rgb2gray(image)
+        compressed_image = cv2.resize(gray_image, (image_size, image_size))
+        reshaped_image = compressed_image.reshape(-1, image_size, image_size, 1)
+        normalized_image = (reshaped_image / 255) - 0.5
 
-    #print the actual letter instead of percentages
-    predicted_letter = list(prediction[0]).index(max(prediction[0]))
-    print(prediction)
-    print(letters[predicted_letter])
-  
-#if capture went wrong
+        #predict the letter
+        prediction = model.predict(normalized_image)
+
+        #print the actual letter instead of percentages
+        predicted_letter = list(prediction[0]).index(max(prediction[0]))
+        print(prediction)
+        print(letters[predicted_letter])
+    
+    #if capture went wrong
+    else:
+        print("No image was captured")
+
+
 else:
-    print("No image was captured")
+    #for each letter
+    for i in range(len(letters) - 3):
+        #open the test image version of the letter
+        path_captured = "C:/Users/emmar/Documents/GitHub/VISN/Eindopdracht/dataset/asl_alphabet_test/als_alphabet_test_captures/"+letters[i]+"_test.jpg"
+        path = "C:/Users/emmar/Documents/GitHub/VISN/Eindopdracht/dataset/asl_alphabet_test/asl_alphabet_test/"+letters[i]+"_test.jpg"
+        image_to_predict = cv2.imread(path_captured, cv2.IMREAD_GRAYSCALE)
 
-#load_model van checkpoint om model op te slaan en weer te loaden, zodat niet bij opstarten model te trainen
+        #preprocess the image the same as images used for training
+        compressed_image_to_predict = cv2.resize(image_to_predict, (image_size, image_size))
+        reshaped_image_to_predict = compressed_image_to_predict.reshape(-1, image_size, image_size, 1)
+        normalized_image_to_predict = (reshaped_image_to_predict / 255) - 0.5
+
+        #predict the letter
+        prediction = model.predict(normalized_image_to_predict)
+
+        #print the actual letter instead of percentages
+        predicted_letter = list(prediction[0]).index(max(prediction[0]))
+        # print(prediction)
+        print(letters[predicted_letter])
